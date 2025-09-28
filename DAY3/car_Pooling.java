57a861704aa312c540551c34b0e959a6963393bb
Khushi Gangwar <khushigangwar344@gmail.com>
2025-09-28 13:02:42 +0530

    day3

---
+++ DAY3/car_Pooling.java
@@ -0,0 +1,29 @@
+package DAY3;
+
+
+
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        
+        int[] stops = new int[1001]; // problem constraints: locations 0..1000
+
+        
+        for (int i = 0; i < trips.length; i++) {
+            int numPass = trips[i][0];
+            int from = trips[i][1];
+            int to = trips[i][2];
+
+            stops[from] += numPass;
+            stops[to] -= numPass;
+        }
+
+        
+        int curr = 0;
+        for (int i = 0; i < 1001; i++) {
+            curr += stops[i];
+            if (curr > capacity) return false;
+        }
+
+        return true;
+    }
+}
